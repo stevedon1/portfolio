d272cf74e23f4aa8fc787a04402608400ac2a189
Steve <stevowny@gmail.com>
2025-01-20 18:07:10 +0300

    fix bug

--- app/contact-me/page.tsx
+++ app/contact-me/page.tsx
@@ -39,6 +39,24 @@ export default function ContactMe() {
           I would love to hear from you! Feel free to reach out with any questions, ideas, or opportunities.
         </p>
       </div>
+      <div>
+          <a
+            href="https://wa.me/254114228361"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-green-400 p-8 hover:text-green-500 transition"
+          >
+            <FontAwesomeIcon icon={faWhatsapp} size="2x" />
+          </a>
+          <a
+            href="https://www.linkedin.com/in/steve-dongich"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-blue-600 p-8 hover:text-blue-700 transition"
+          >
+            <FontAwesomeIcon icon={faLinkedin} size="2x" />
+          </a>
+      </div>
       <form
         className="space-y-4 w-full max-w-lg bg-white p-6 rounded-lg shadow-md"
         ref={formRef}
